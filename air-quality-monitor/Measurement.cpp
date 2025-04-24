97e3ca0b3bf5747832962b197dbb1344892c6c6c
Dreeewien <120193142+Dreeewien@users.noreply.github.com>
2025-04-24 17:02:42 +0200

    Delete air-quality-monitor directory

--- air-quality-monitor/Measurement.cpp
+++
@@ -1,7 +0,0 @@
-#include "Measurement.h"
-using namespace std;
-
-Measurement::Measurement() : value(0.0) {}
-
-Measurement::Measurement(const string& date, double value)
-    : date(date), value(value) {}
--- air-quality-monitor/Sensor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Sensor.h"
-
-using namespace std;
-
-Sensor::Sensor() {}
-
-Sensor::Sensor(int id, int stationId, const Parameter& param)
-    : id(id), stationId(stationId), param(param) {}
